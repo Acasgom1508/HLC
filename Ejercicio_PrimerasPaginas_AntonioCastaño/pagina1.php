bf1796bd67a30718d1b073b08ca48db68bb5032e
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-10-07 18:42:24 +0200

    Hecho parte 1 del ejercicio 1

---
+++ Ejercicio_PrimerasPaginas_AntonioCastaño/pagina1.php
@@ -0,0 +1,3 @@
+<?php
+echo "<h1>Esto es la página 1</h1>"; //Indicamos que es la página 1
+echo '<a href="./pagina2.php"> Ir a Página 2 <a/>'; //Añadimos el enlace a la página 2
---
+++ Ejercicio_PrimerasPaginas_AntonioCastaño/pagina2.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1>Esto es la página 2</h1>"; //Indicamos en la páguina en la que nos encontramos
+echo "¡Hola, mundo!<br>"; //Hacemos el hola mundo y añadimos un salto de linea
+echo '<a href="./pagina1.php"> Ir a Página 1 <a/>'; //Terminamos con el enlace a la página 1
