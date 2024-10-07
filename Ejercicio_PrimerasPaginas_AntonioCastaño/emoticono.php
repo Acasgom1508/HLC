b65ad0b0a2305ca7e0cfc471cfdd81b8909f02f4
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-10-07 19:23:02 +0200

    Termino el apartado 4 del ejercicio 1

---
+++ Ejercicio_PrimerasPaginas_AntonioCastaño/emoticono.php
@@ -0,0 +1,3 @@
+<?php
+$codigo_emoticono = rand(128512, 128586); //Creo el código del emoticono
+print "<p>Emoticono &#$codigo_emoticono con código $codigo_emoticono</p>";//Muestro por pantalla el emoticono con su código correspondiente
