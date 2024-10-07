8e7a69fe77a0240c355492b0d5236c2d5e8c40f2
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-10-07 19:22:43 +0200

    Comento el codigo de codigo_color.php

--- Ejercicio_PrimerasPaginas_AntonioCastaño/codigo_color.php
+++ Ejercicio_PrimerasPaginas_AntonioCastaño/codigo_color.php
@@ -1,7 +1,6 @@
 <?php
-$red = rand(0,255);
-$green = rand(0,255);
-$blue = rand(0,255);
+$red = rand(0,255);//Genero la cantidad de rojo
+$green = rand(0,255);//Genero la cantidad de verde
+$blue = rand(0,255);//Genero la cantidad de azul
 
-print "<p>rgb($red, $green, $blue)</p>";
-
+print "<p>rgb($red, $green, $blue)</p>";//Muestro el codigo rgb por pantalla
