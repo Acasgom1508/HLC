4ddfacc6ae6b75a9de3e7d660b55c49d65208cec
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-10-08 08:50:50 +0200

    Update codigo_color.php

--- Ejercicio_PrimerasPaginas_AntonioCastaño/codigo_color.php
+++ Ejercicio_PrimerasPaginas_AntonioCastaño/codigo_color.php
@@ -3,4 +3,4 @@ $red = rand(0,255);//Genero la cantidad de rojo
 $green = rand(0,255);//Genero la cantidad de verde
 $blue = rand(0,255);//Genero la cantidad de azul
 
-print "<p>rgb($red, $green, $blue)</p>";//Muestro el codigo rgb por pantalla
+print "<p style='color: rgb($red, $green, $blue)'>rgb($red, $green, $blue)</p>";//Muestro el codigo rgb por pantalla
